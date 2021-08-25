ec6cf2255c88704c616a3aea13dd60c966071a9b
skopinmi <64150928+skopinmi@users.noreply.github.com>
2021-08-25 21:33:47 +0300

    hw

--- hw8/scripts/basket.js
+++ hw8/scripts/basket.js
@@ -2,10 +2,23 @@
 let basket = {};
 let btnsAddToBasket = document.querySelectorAll("button#addToCart");
 let countProduct = document.querySelector(".cartIconWrap span");
+let basketEl = document.querySelector(".rightHeader");
+let table = document.querySelector(".basket");
 
 btnsAddToBasket.forEach(function (btn) {
     btn.addEventListener("click", addToBasket);
 });
+
+basketEl.addEventListener("mouseover", basketVisible);
+basketEl.addEventListener("mouseout", basketInvisible);
+
+function basketVisible() {
+    table.style.display = "block";
+}
+
+function basketInvisible() {
+    table.style.display = "none";
+}
 
 function addToBasket(event) {
     let newProductId = event.currentTarget.getAttribute("data-productId");
